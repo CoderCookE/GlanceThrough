290214422944ef3a6eb35e3b7a52337c4c2826e2
CoderCookE <ejcook111@gmail.com>
2014-03-11 11:56:31 -0400

    bookmarklet

--- glancethroughbookmarklet.js
+++ glancethroughbookmarklet.js
@@ -3,7 +3,7 @@ if ( document.readyState === "complete" ) {
 	jqueryInject.setAttribute('src','http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js');
 	document.getElementsByTagName('body')[0].appendChild(jqueryInject);
 	var bPopupInject = document.createElement('script');
-	bPopupInject.setAttribute('src','https://raw.github.com/dinbror/bpopup/master/jquery.bpopup.min.js');
+	bPopupInject.setAttribute('src','https://rawgithub.com/dinbror/bpopup/master/jquery.bpopup.min.js');
 	document.getElementsByTagName('body')[0].appendChild(bPopupInject);
 
 	var foundArticle = ('http://glancethrough.info/articles/bookmarklet?url='+ encodeURIComponent(window.location));
@@ -19,15 +19,19 @@ if ( document.readyState === "complete" ) {
 	articleIframe.setAttribute('id', 'dialog');
 	articleIframe.setAttribute('width', '80%');
 	articleIframe.setAttribute('height', '350px');
-
-	setTimeout(function(){
+	var openPopup = function(){
+		if(typeof $("#dialog").bPopup !== "undefined") {
 	document.getElementsByTagName('body')[0].appendChild(articleIframe);
 
 	$('#dialog').bPopup({
 		followSpeed: 'fast',
 		onClose: function() {$('#iframeDiv').remove()}
 		});
-	},400)
+	  } else {
+	  	setTimeout(openPopup, 400);
+	  }
+	}
+	setTimeout(openPopup,400)
 }else{
 	alert("Please wait for page to load and try again.")
 }
